4799e012bf0591e55671eb5bc6009ebb7bd47f91
CaptiveIT1 <CaptiveIT1@CAPTIVEIT1-PC>
2020-02-17 09:00:08 +0800

    Add project files.

---
+++ PrinterFile/Main.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PrinterFile
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(78, 75);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(92, 35);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Extract";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtYear
+            // 
+            this.txtYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYear.Location = new System.Drawing.Point(96, 30);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(117, 29);
+            this.txtYear.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Year :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 230);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Main";
+            this.Text = "Extract Text File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PrinterFile/Main.cs
@@ -0,0 +1,113 @@
+﻿using Bond.IO.Unsafe;
+using Ionic.Zip;
+//using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+//using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrinterFile
+{
+    public partial class Main : Form
+    {
+     
+        public Main()
+        {
+            InitializeComponent();
+        }
+        public int counter = 0;
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string source = @"O:\Zips\Metro\" + txtYear.Text + @"\";
+            string[] pdfFiles = GetFileNames(source, "*.zip");
+            string target = "D:\\ExtractedTextFile\\";
+            {
+                Directory.CreateDirectory(target + txtYear.Text);
+
+            }
+
+            string destination = target + txtYear.Text + "\\";
+
+
+            for (int i = 0; i < counter; i++)
+            {
+                {
+                    Directory.CreateDirectory(destination + pdfFiles[i]);
+
+                }
+
+                string destination1 = destination + pdfFiles[i];
+
+                using (ZipFile zip2 =ZipFile.Read(source+pdfFiles[i]))
+                {
+                    foreach (ZipEntry item in zip2)
+                    {
+                        if(item.FileName.EndsWith(".txt"))
+                        {
+
+                            item.Extract(destination1);
+                           // MessageBox.Show("Files has been extracted!!");
+                        }
+
+                    }
+                 
+                }
+                
+            }
+            MessageBox.Show("Files has been Extrated in this File Folder : " + target);
+            //using (ZipFile zip = ZipFile.Read(source + pdfFiles[i]))
+            //{
+
+            //    //  string[] pdfFiles1 = GetFileNames(@"O:\Zips\Metro\2020\"+folderName, "*.txt");
+
+            //    ZipEntry zip1 = zip[folderName + "\\" + textFileName + ".txt"];
+            //    //foreach (ZipEntry zip1 in zip)
+            //    //{
+
+            //    zip1.Extract(target);
+            //    //}
+
+            //}
+        }
+        private  string[] GetFileNames(string path, string filter)
+        {
+            string[] files = Directory.GetFiles(path, filter);
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = Path.GetFileName(files[i]);
+                counter++;
+            }
+            
+            return files;
+        }
+        private string[] GetFolderName(string path, string filter)
+        {
+            string[] files = Directory.GetFiles(path, filter);
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = Path.GetFileName(files[i]);
+                counter++;
+            }
+
+            return files;
+        }
+        private string[] GetTextFileName(string path, string filter)
+        {
+            string[] files = Directory.GetFiles(path, filter);
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = Path.GetFileName(files[i]);
+                counter++;
+            }
+
+            return files;
+        }
+    }
+}
